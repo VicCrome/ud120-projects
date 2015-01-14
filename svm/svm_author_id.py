4e586ed47889b665d179f2b3b5368388df808f32
vcrome <vcrome@hotmail.com>
2015-01-14 06:56:46 -0500

    Complete SVM method

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,7 +12,7 @@
     
 import sys
 from time import time
-sys.path.append("../tools/")
+sys.path.append("C:\\Users\\Victor\\Desktop\\Udacity\\introml\\ud421-projects\\tools")
 from email_preprocess import preprocess
 
 
@@ -21,11 +21,36 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
+### features_train = features_train[:len(features_train)/100] 
+### labels_train = labels_train[:len(labels_train)/100] 
 
 
 #########################################################
-### your code goes here ###
+from sklearn.svm import SVC
+ 
+### create classifier
+clf = SVC(kernel="rbf",C=10000.0)
+
+### fit the classifier on the training features and labels
+t0 = time()
+clf.fit(features_train,labels_train)
+print "training time: ", round(time()-t0,3), "s"
+
+### use the trained classifier to predict labels for the test features
+t0 = time()
+pred = clf.predict(features_test)
+print "prediction time: ", round(time()-t0,3), "s"
+
+for thing in [10,26,50]:
+    print pred[thing]
+    
+print sum(pred)
+
+### calculate and return the accuracy on the test data
+accuracy = clf.score(features_test,labels_test)
+print "Accuracy is: ", accuracy
+
+    
 
 #########################################################
 
