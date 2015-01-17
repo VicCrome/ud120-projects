d3778337ef45b4faab5609c17597a69717e390ac
vcrome <vcrome@hotmail.com>
2015-01-17 16:12:46 -0500

    Add linear regression method

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -16,9 +16,9 @@
 
 import sys
 import pickle
-sys.path.append("../tools/")
+sys.path.append("C:\\Users\\Victor\\Desktop\\Udacity\\introml\\ud421-projects\\tools")
 from feature_format import featureFormat, targetFeatureSplit
-dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
+dictionary = pickle.load( open("C:\\Users\\Victor\\Desktop\\Udacity\\introml\\ud421-projects\\final_project\\final_project_dataset_modified.pkl", "r") )
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
@@ -30,19 +30,23 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
 ### your regression goes here!
 ### please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly
+from sklearn.linear_model import LinearRegression
 
+reg = LinearRegression()
+reg.fit(feature_train,target_train)
 
+print reg.coef_[0]
+print reg.intercept_
 
-
-
-
+print reg.score(feature_train,target_train)
+print reg.score(feature_test,target_test)
 
 
 ### draw the scatterplot, with color-coded training and testing points
@@ -64,6 +68,12 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="r") 
+print reg.coef_
+
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
