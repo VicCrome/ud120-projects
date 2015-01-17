4a69fcff45430529a7cc5cc0f5be7174eb52a754
vcrome <vcrome@hotmail.com>
2015-01-17 15:27:14 -0500

    Add Adaboost in Choose Your Own method

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -31,9 +31,14 @@ plt.show()
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
+from sklearn.ensemble import AdaBoostClassifier
 
 
+clf = AdaBoostClassifier(n_estimators=50)
+clf.fit(features_train,labels_train)
 
+acc = clf.score(features_test, labels_test)
+print "Your accuracy is: ", acc
 
 
 
