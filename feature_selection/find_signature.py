44aab498a044212ab83eb2b9bf3d609b8b145923
vcrome <vcrome@hotmail.com>
2015-01-24 16:30:36 -0500

    Add feature selection exercise - note my indices seem to mismatch from udacity

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python
 
+import nltk
 import pickle
 import numpy
 numpy.random.seed(42)
 
+stopset = set(nltk.corpus.stopwords.words('english'))
+stopset = stopset | {'sshacklensf'}
+stopset = stopset | {'cgermannsf'}
+
+
+
+
 
 ### the words (features) and authors (labels), already largely processed
-words_file = "word_data_overfit.pkl" ### like the file you made in the last mini-project 
-authors_file = "email_authors_overfit.pkl"  ### this too
+words_file = "C:\\Users\\Victor\\Desktop\\Udacity\\introml\\ud421-projects\\feature_selection\\word_data_overfit.pkl" ### like the file you made in the last mini-project 
+authors_file = "C:\\Users\\Victor\\Desktop\\Udacity\\introml\\ud421-projects\\feature_selection\\email_authors_overfit.pkl"  ### this too
 word_data = pickle.load( open(words_file, "r"))
 authors = pickle.load( open(authors_file, "r") )
 
@@ -20,7 +28,7 @@ features_train, features_test, labels_train, labels_test = cross_validation.trai
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
-                             stop_words='english')
+                             stop_words=stopset)
 features_train = vectorizer.fit_transform(features_train).toarray()
 features_test  = vectorizer.transform(features_test).toarray()
 
@@ -34,6 +42,18 @@ labels_train   = labels_train[:150]
 
 
 ### your code goes here
+from sklearn.tree import DecisionTreeClassifier
 
+clf = DecisionTreeClassifier()
+clf.fit(features_train,labels_train)
 
+print clf.score(features_test, labels_test)
+
+importances = clf.feature_importances_
+
+#print importances[importances >0.2]
+
+print [(i,j) for i,j in enumerate(importances) if j > 0.2]
+
+print vectorizer.get_feature_names()[14366]
 
