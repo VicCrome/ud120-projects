065aae124a9abfe78022301af77473b9e9e6fff5
vcrome <vcrome@hotmail.com>
2015-01-24 14:24:53 -0500

    Edited various times to pull different pieces of data

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,7 +19,7 @@ import pickle
 
 enron_data = pickle.load(open("C:\\Users\\Victor\\Desktop\\Udacity\\introml\\ud421-projects\\final_project\\final_project_dataset.pkl", "r"))
 
-print len(enron_data)
+#print len(enron_data)
 
 #print enron_data.keys()
 #print enron_data['SKILLING JEFFREY K']['total_payments']
@@ -30,25 +30,25 @@ print len(enron_data)
 print enron_data['GLISAN JR BEN F']
 #print len(enron_data['GLISAN JR BEN F'])
 
-email_count = 0
-num_sal_count = 0
-tot_payments_count = 0
-poi_count = 0
-poi_NaN_count = 0
-for key in enron_data:
-    if enron_data[key]['poi']:
-        poi_count+=1
-    if (enron_data[key]['poi'] and enron_data[key]['total_payments'] == 'NaN'):
-        poi_NaN_count+=1
-    if enron_data[key]['total_payments'] == 'NaN':
-        tot_payments_count+=1
-#    if enron_data[key]['email_address'] != 'NaN':
-#        email_count+=1
-#    if enron_data[key]['salary'] != 'NaN':
-#        num_sal_count+=1
-#        
-print tot_payments_count
-#print email_count
-#print num_sal_count
-print poi_NaN_count
-print poi_count
+#email_count = 0
+#num_sal_count = 0
+#tot_payments_count = 0
+#poi_count = 0
+#poi_NaN_count = 0
+#for key in enron_data:
+#    if enron_data[key]['poi']:
+#        poi_count+=1
+#    if (enron_data[key]['poi'] and enron_data[key]['total_payments'] == 'NaN'):
+#        poi_NaN_count+=1
+#    if enron_data[key]['total_payments'] == 'NaN':
+#        tot_payments_count+=1
+##    if enron_data[key]['email_address'] != 'NaN':
+##        email_count+=1
+##    if enron_data[key]['salary'] != 'NaN':
+##        num_sal_count+=1
+##        
+#print tot_payments_count
+##print email_count
+##print num_sal_count
+#print poi_NaN_count
+#print poi_count
